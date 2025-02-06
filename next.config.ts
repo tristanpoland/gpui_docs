d6911331597fd6da18e81ee794c9703872c32d05
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-02-06 12:57:53 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/gpui_docs",
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
